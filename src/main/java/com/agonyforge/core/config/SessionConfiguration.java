63f8687b2e39d57bae2611c5505659ca0cb2487a
Peter Keeler <peter@bonevm.com>
2022-08-31 22:33:56 -0700

    don't use JSP taglibs, use Spring security context from FreeMarker directly

--- src/main/java/com/agonyforge/core/config/SessionConfiguration.java
+++ src/main/java/com/agonyforge/core/config/SessionConfiguration.java
@@ -1,9 +1,21 @@
 package com.agonyforge.core.config;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
-@EnableJdbcHttpSession(maxInactiveIntervalInSeconds = 86400)
+@EnableHazelcastHttpSession
 @Configuration
 public class SessionConfiguration {
+    @Bean
+    public HazelcastInstance hazelcastInstance() {
+        Config config = new Config();
+
+        config.setClusterName("agony-forge-sessions");
+
+        return Hazelcast.newHazelcastInstance(config);
+    }
 }
